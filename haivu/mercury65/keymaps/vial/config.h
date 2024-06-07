508e9672c30e718f55ba5172f098c7d13dd877f9
George <64863990+HaivuUK@users.noreply.github.com>
2024-06-07 13:52:40 +0100

    Add files via upload

---
+++ haivu/mercury65/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x82, 0x44, 0x42, 0x6A, 0x9A, 0xBA, 0x57, 0x0D}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
