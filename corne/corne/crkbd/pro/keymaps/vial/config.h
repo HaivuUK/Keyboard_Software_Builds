16ca330a0efceb889e24a1a82ad18dbd0b91204b
George <64863990+HaivuUK@users.noreply.github.com>
2025-08-12 13:54:18 +0100

    Added the corne and ergotravel
    added the corne with converted mechboards firmware
    added the ergotravel with vial firmware and documentation

---
+++ corne/corne/crkbd/pro/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+// Copyright 2025 Dasky (@daskygit)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "keyboards/mechboards/common/rgb_effects.h"
+
+#define VIAL_KEYBOARD_UID {0xC2, 0x71, 0xF3, 0xFF, 0xA4, 0xDA, 0x06, 0x0A}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
---
+++ ergotravel/ergotravel/keymaps/vial/config.h
@@ -0,0 +1,28 @@
+/*
+Copyright 2017 Pierre Constantineau <jpconstantineau@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA6, 0xAA, 0xA6, 0x22, 0xC9, 0x7B, 0x8E, 0x35}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#undef USE_I2C
+#define USE_SERIAL
+#define MASTER_LEFT
+#define USE_SERIAL_PD2
